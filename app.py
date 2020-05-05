2443ecc9aae8679064d0ae9caad2309a2bd64860
Arun Vigesh V G <arunvigesh1999@gmail.com>
2020-05-05 23:21:54 +0530

    Add files via upload

---
+++ app.py
@@ -0,0 +1,11 @@
+from flask import Flask
+from predictor import Predictor
+
+app = Flask(__name__)
+
+@app.route('/')
+def home():
+    return Predictor().predict()
+
+if __name__ == '__main__': 
+    app.run()
