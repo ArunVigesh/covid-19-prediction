430a0a3a763e153fb5f23072646dc0110e1b8389
MANOJKUMAR VK <vkmanojk@gmail.com>
2020-05-06 12:54:26 +0530

    Update app.py

--- app.py
+++ app.py
@@ -3,10 +3,12 @@ from predictor import Predictor
 
 app = Flask(__name__)
 
+def predict():
+    return Predictor().predict()
 @app.route('/')
 def home():
     print('Predicting')
-    return Predictor().predict()
+    return '<h1> Corona Predictor </h1>', predict()
  
 if __name__ == '__main__': 
     app.run()
